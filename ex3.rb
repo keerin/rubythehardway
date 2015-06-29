8ebdf498f72f7a250156d00179a88edc3ce0ab8e
keerin <thekieran@gmail.com>
2015-06-29 22:54:02 +0100

    First three exercises

---
+++ ex3.rb
@@ -0,0 +1,28 @@
+# This line prints a string
+puts "I will now count my chickens:"
+# This line prints a string with an operation embedded, and will show the result of the operation
+puts "Hens #{25.0 + 30.0 / 6.0}"
+# This line prints a string with an operation embedded, and will show the result of the operation
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+# This line prints a string
+puts "Now I will count the eggs:"
+# This line print the results of an operation
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+# This line prints a string
+puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+# This line print the results of an operation
+puts 3.0 + 2.0  < 5.0 - 7.0
+# This line prints a string with an operation embedded, and will show the result of the operation
+puts "What is 3.0 + 2.0? #{3.0 + 2.0}"
+# This line prints a string with an operation embedded, and will show the result of the operation
+puts "What is 5.0 - 7.0? #{5.0 - 7.0}"
+# This line prints a string
+puts "Oh, that's why it's false."
+# This line prints a string
+puts "How about some more."
+# This line prints a string with an operation embedded, and will show the result of the operation
+puts "Is it greater? #{5.0 > -2.0}"
+# This line prints a string with an operation embedded, and will show the result of the operation
+puts "Is it  greater or equal? #{5.0 >= -2.0}"
+# This line prints a string with an operation embedded, and will show the result of the operation
+puts "Is it less or equal? #{ 5.0 <= -2.0}"
