97b3e7524f99002cd80159db23901aa2e7bea1c6
keerin <thekieran@gmail.com>
2015-07-04 16:29:03 +0100

    Re-adding ex13.rb because it was not correct before.

--- ex13.rb
+++ ex13.rb
@@ -3,3 +3,9 @@ first, second, third = ARGV
 puts "Your first variable is: #{first}"
 puts "Your second variable is: #{second}"
 puts "Your third variable is: #{third}"
+
+puts "would you like to add more text?"
+answer = $stdin.gets.chomp()
+puts "So, you said #{answer}?"
+
+# I have used $stdin, and not ARGV. This is advised here - http://rorwebprogramming.blogspot.co.uk/2014/08/argv-and-getschomp.html - I used this because ARGV.gets.chomp doesnt work. The asnswr to why this works is in here in the comment by rampion - https://www.reddit.com/r/ruby/comments/2b9xvl/stdin_and_argv/
