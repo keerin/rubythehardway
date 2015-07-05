765948c5a56c9a7df57c7035b7c19a59b9359bc5
keerin <thekieran@gmail.com>
2015-07-05 17:06:42 +0100

    Adding ex19.rb including study drills.

---
+++ ex19.rb
@@ -0,0 +1,28 @@
+def cheese_and_crackers(cheese_count, boxes_of_crackers)
+    puts "You have #{cheese_count} cheeses!"
+    puts "You have #{boxes_of_crackers} boxes of crackers!"
+    puts "Man that's enough for a party!"
+    puts "Get a blanket.\n"
+end
+
+puts "We can just give the function numbers directly:"
+cheese_and_crackers(20,30)
+
+puts "OR, we can use the variables from our script:"
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+puts "We can even do math inside too:"
+cheese_and_crackers(10 + 20, 5 + 6)
+
+puts "And we can combine the two, variables and maths:"
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
+
+puts "How much cheese do you have?"
+amount_of_cheese = $stdin.gets.chomp.to_i
+
+puts "and how many crackers do you have?"
+amount_of_crackers = $stdin.gets.chomp.to_i
+puts cheese_and_crackers(amount_of_cheese, amount_of_crackers)
