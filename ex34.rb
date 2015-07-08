89cbf907ac8025c721df426229c556e90723c626
keerin <thekieran@gmail.com>
2015-07-08 08:47:36 +0100

    Exercise 34 completed on the train in the morning

---
+++ ex34.rb
@@ -0,0 +1,12 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+animal_at_one = animals[1]
+third_animal = animals[2]
+first_animal = animals[0]
+animal_at_three = animals[3]
+fifth_animal = animals[4]
+animal_at_two = animals[2]
+sixth_animal = animals[5]
+animal_at_four = animals[4]
+
+puts animal_at_one, third_animal, first_animal, animal_at_three, fifth_animal, animal_at_two, sixth_animal, animal_at_four
