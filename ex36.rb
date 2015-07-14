3655a030a53e568e38dfae3f194af1016ce6f021
keerin <thekieran@gmail.com>
2015-07-14 22:25:14 +0100

    More progress on ex36 game. Have done caves.

--- ex36.rb
+++ ex36.rb
@@ -1,7 +1,54 @@
-# Placeholder so this is not an empty file. The instructions for this exercise are here - http://learnrubythehardway.org/book/ex36.html
+# The instructions for this exercise are here - http://learnrubythehardway.org/book/ex36.html
+
+def cave_jump
+    dead("Jumping as high as you can, you attempt to clear the rock. You can't jump very high. Kobolds have incredibly small and weak legs. You face plant into the rocks, which then roll over your stupid body.")
+end
+
+def cave_dive
+    puts "You dive to safety, pushing one of your fellow Kobolds in the path of the falling rocks! Ah well, better him than you. There is a light above you. Climbing a little, you pop your head above ground. Applebee Farm!"
+end
+
+def cave_right
+    puts "Rocks are falling! What will you do?"
+    puts "Will you dive through the crowded Kobolds beside you to safety? If so, type 'dive'."
+    puts "You could always try to do some Indiana Jones shit and jump over the rocks hutling towards you? If you want to try this, type 'jump'."
+
+    print "> "
+    cave_choice = $stdin.gets.chomp
+
+    if cave_choice.include? "jump"
+        cave_jump
+    elsif cave_choice.include? "dive"
+        cave_dive
+    else 
+        cave_right
+    end
+end
+
+def cave_left
+    puts "BATS!\n\nYou've walked straight into what seems like a cloud made entirely of bats!"
+    puts "You can hardly see through the hundreds of bats fkying all around you, and there is no sound except the flapping of their wings and high-ptched screech."
+    puts "While you flail your arms wildly to get the bats out of your face, the other Kobolds are doing the same."
+    puts "SMACK\n\n You are punched in the face by a flailing Kobold and are instantly knocked out. The Kobolds around you are completely oblivious to this and accidentally kick you in the face repeatedly, until you die."
+    dead("Kobolds are cruel and stupid, but it takes a special Kobold to be both at the same time. Unforunately you ended up with three cruel and stupid Kobolds, and wound up dead.")
+end
 
 def cave
-    puts "This is the cave function."
+    puts "Entering the tunnels which lead towards the farm, you and the others are in high spirits. As you were picked first by the King, the other Kobolds have made your their defacto leader. Your choices will decide wether your mission is sucessful, or if you will lead everyone to their deaths."
+    puts "After half an hour of walking and excited chatter, you come to a fork in the cave. To the left, the sound of wind can be heard past the short and narrow entrance. To the right, no sounds can be heard, but the entrance seems wider and taller. Both are completely pitch black. Luckily, Kobolds can pretty much see in the dark."
+    puts "Type 'left' to take that tunnel, and 'right' to take the other."
+
+    print "> "
+
+    cave_direction = $stdin.gets.chomp
+
+    if cave_direction.include? "left"
+        cave_left
+    elsif cave_direction.include? "right"
+        cave_right
+    else
+        cave
+    end
 end
 
 def dead(why)
